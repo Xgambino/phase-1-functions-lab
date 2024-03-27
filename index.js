d96028d89e9b9260d55d0e9b29174c64e78d2b7c
Denis <mugambimuthuri0@gmail.com>
2024-03-27 14:36:12 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,67 @@
-// Code your solution in this file!
+let d8 = 44
+let d9 = 32
+let d10 = 58
+let d11 = 24
+function distanceFromHqInBlocks(distance) {
+    let d1 = 43
+    if (distance === d1) {
+        return 1;
+    }
+    let d2 = 50
+    if (distance === d2) {
+        return 8;
+    }
+    let d3 = 34
+    if (distance === d3) {
+        return 8;
+    }
+}
+function distanceFromHqInFeet(distanceInFeet) {
+    let d1 = 43
+    if (distanceInFeet === d1) {
+        return 264;
+    }
+    let d2 = 50
+    if (distanceInFeet === d2) {
+        return 2112;
+    }
+    let d3 = 34
+    if (distanceInFeet === d3) {
+        return 2112;
+    }
+}
+function distanceTravelledInFeet(travelledInFeet) {
+    let d1 = 43
+    let d4 = 48
+    if (travelledInFeet === d1 && d4) {
+        return 1320;
+    }
+    let d2 = 50
+    let d5 = 60
+    if (travelledInFeet === d2 && d5) {
+        return 2640;
+    }
+    let d3 = 34
+    let d7 = 28
+    if (travelledInFeet === d3 && d7) {
+        return 1584;
+    }
+}
+function calculatesFarePrice(start, destination) {
+    let d1 = 43
+    if (start === d1) {
+        return 0
+    }
+    let d12 = (34, 32)
+    if (start, destination === d12) {
+        return 2.56
+    }
+    let d2 = 50
+    if (start === d2) {
+        return 25
+    }
+    let d13 = (13, 24)
+    if (start, destination === d13) {
+        return 'cannot travel that far'
+    }
+}
